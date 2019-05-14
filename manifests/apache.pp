7d67b1da13486440651ac0680b1507025f3c0edd
cvquesty <questy@gmail.com>
2019-05-14 16:20:58 -0400

    adding in php

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,5 +1,18 @@
 class sscg_profiles::apache {
 
-  class { 'apache': }
+  class { 'apache':
+    default_vhost => false,
+    mpm_module    => 'prefork',
+  }
+
+  class { '::apache::mod::php': }
+
+  # Ensure subdirectories always get owned by Apache
+  file { '/var/www/html':
+    ensure  => 'directory',
+    owner   => 'apache',
+    group   => 'apache',
+    recurse => true,
+  }
 
 }
