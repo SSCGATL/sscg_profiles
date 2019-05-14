052db90844b5ef9c208e1efe66c691b9ae0ccb9a
cvquesty <questy@gmail.com>
2019-05-14 16:07:32 -0400

    removing password after provisioning

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,8 +1,5 @@
 # MySQL profile
 class sscg_profiles::mysql {
 
-  class { 'mysql::server':
-    root_password           => 'B@sslin3',
-    remove_default_accounts => true,
-  }
+  class { 'mysql::server': }
 }
