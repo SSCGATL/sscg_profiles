fd1876d42674321abdc9209f21f9ea8d73778096
cvquesty <questy@gmail.com>
2019-05-15 13:38:31 -0400

    troubleshooting

--- manifests/hardening.pp
+++ manifests/hardening.pp
@@ -1,9 +1,6 @@
 # Profile to manage the base hardening of the server
 class sscg_profiles::hardening {
 
-  # Filesystem Modules to remove
-  $fs_modules = [ 'cramfs','freevxfs','jffs2','hfs','hfsplus','squashfs','udf','vfat' ]
-
   # Turn off unneeded Filesystem Support
   file { '/etc/modprobe.d/CIS.conf':
     ensure => 'present',
@@ -11,15 +8,6 @@ class sscg_profiles::hardening {
     group  => 'root',
     mode   => '0644',
     source => 'puppet:///modules/sscg_profiles/CIS.conf',
-  }
-
-  # Remove the filesystem kernel modules from the server
-  $fs_modules.each | String $module | {
-    exec { 'remove_fsmodules':
-      path    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
-      command => "/usr/sbin/rmmod ${module}",
-      require => File['/etc/modprobe.d/CIS.conf'],
-    }
   }
 
 }
