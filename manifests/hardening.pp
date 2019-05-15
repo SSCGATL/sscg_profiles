1e7df615651f85b8315f2293d193e17c53bb9b98
cvquesty <questy@gmail.com>
2019-05-15 13:29:41 -0400

    completing the disabling and removal of unneeded filesystem kernel modules

--- manifests/hardening.pp
+++ manifests/hardening.pp
@@ -1,6 +1,9 @@
 # Profile to manage the base hardening of the server
 class sscg_profiles::hardening {
 
+  # Filesystem Modules to remove
+  $fs_modules = [ 'cramfs','freevxfs','jffs2','hfs','hfsplus','squashfs','udf','vfat' ]
+
   # Turn off unneeded Filesystem Support
   file { '/etc/modprobe.d/CIS.conf':
     ensure => 'present',
@@ -8,6 +11,13 @@ class sscg_profiles::hardening {
     group  => 'root',
     mode   => '0644',
     source => 'puppet:///modules/sscg_profiles/CIS.conf',
+  }
+
+  # Remove the filesystem kernel modules from the server
+  exec { 'remove_fs_modules':
+    path    => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
+    command => "/usr/sbin/rmmod ${fs_modules}",
+    require => File['/etc/modprobe.d/CIS.conf'],
   }
 
 }
